4e34fd401c071e1af5c4af114d4f35830ec51bd9
mr-taifur <taifurshishir123@gmail.com>
2025-03-20 22:47:59 +0600

    problem 1

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TrackerSheet());
+}
+
+class TrackerSheet extends StatelessWidget {
+  const TrackerSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 210, 172, 95),
+      body: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 4, 82, 238),
+            fontFamily: 'DMSans', 
+          ),
+        ),
+      ),
+    );
+  }
+}
